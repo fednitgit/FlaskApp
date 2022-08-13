cd7e1c37c2f6ad22d69f524288d1175049bf7828
fednitgit <97748488+fednitgit@users.noreply.github.com>
2022-08-12 20:01:24 -0400

    Update chartApp.py

--- chartApp.py
+++ chartApp.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def chart():
-labels = ["January","February","March","April","May","June","July","August"]
-values = [10,9,8,7,6,4,7,8]
-return render_template('chart.html', values=values, labels=labels)
+  labels = ["January","February","March","April","May","June","July","August"]
+  values = [10,9,8,7,6,4,7,8]
+  return render_template('chart.html', values=values, labels=labels)
 
 if __name__ == "__main__":
-app.run(host='0.0.0.0', port=5001)
+  app.run()
